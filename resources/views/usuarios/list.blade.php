cc2dfdb82f72da3ddbc015aa57e21a456458d822
brunosilva0603 <bruunosrs@gmail.com>
2020-11-23 19:32:36 -0300

    Atualização de layout de lista

--- resources/views/usuarios/list.blade.php
+++ resources/views/usuarios/list.blade.php
@@ -9,14 +9,43 @@
 
                 <div class="card-body">
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
                     @endif
 
                     <h1>Lista dos usuarios</h1>
                     @foreach( $usuarios as $u )
-                    <p>{{ $u->user_id }} | {{ $u->name }} | {{ $u->login }} | {{ $u->password }} | {{ $u->status }}</p>
+                    <table class="table">
+                        <thead class="thead-light">
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">First</th>
+                                <th scope="col">Last</th>
+                                <th scope="col">Handle</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                                <td>Thornton</td>
+                                <td>@fat</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td>Larry</td>
+                                <td>the Bird</td>
+                                <td>@twitter</td>
+                            </tr>
+                        </tbody>
+                    </table>
                     @endforeach
                 </div>
             </div>
